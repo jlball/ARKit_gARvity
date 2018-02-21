4dd579f4f67fc066df3d863b0b1e0f2f5f71efb5
grabermtw <grabermtw@gmail.com>
2018-02-20 23:52:56 -0500

    Ship PC compatible stuff
    It flies around a bit, try not to die

---
+++ ARKit_gARvity-master/ARKit_gARvity-master/ARKit_gARvity Mk. I/Assets/ShipSpawner.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSpawner : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject Omnipotentv3;
+    public Rigidbody rbAttractor;
+    public float offset = 10.0f;
+    public float debugVel = 10.0f;
+
+    private bool touchLock = false;
+    //private GameObject planetoid;
+    
+    private Vector2 initialTouchPosition;
+    private Vector2 initialMousePostition;
+    private Vector3 spawnPosition;
+    private float touchDeltaY;
+    private bool spawned = false;
+    private bool held = false;
+    //private Touch touch;
+    
+
+    
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+        //TOUCH CONTROL HERE:
+
+        //if (Input.touchCount > 0)
+        //{
+
+        //    touch = Input.GetTouch(0);
+
+        //    switch (touch.phase)
+        //    {
+        //        case TouchPhase.Began:
+
+        //            if (!touchLock)
+        //            {
+
+        //                initialTouchPosition = touch.position;
+
+        //                touchLock = true;
+        //            }
+        //            break;
+
+        //        case TouchPhase.Moved:
+        //            touchDeltaY = initialTouchPosition.y - touch.position.y;
+        //            Debug.Log((touchDeltaY));
+
+        //            break;
+
+        //        case TouchPhase.Ended:
+
+        //            SpawnSpaceship(Mathf.Abs(touchDeltaY / 100.0f));
+
+        //            touchLock = false;
+        //            break;
+        //    }
+
+
+        //Mouse control for testing
+
+        if(Input.GetMouseButton(0) && spawned == false){
+            //initialTouchPosition = Input.mousePosition;
+            spawned = true;
+            //held = true;
+            Debug.Log((initialTouchPosition));
+            SpawnSpaceship(2);
+        }
+       //// while (Input.GetMouseButton(0))
+       //     while (held)
+       //     {
+            
+
+       //     touchDeltaY = initialTouchPosition.y - Input.mousePosition.y;
+       //     Debug.Log((touchDeltaY));
+
+       //     if (!Input.GetMouseButton(0))
+       //     {
+       //         held = false;
+       //         SpawnSpaceship(Mathf.Abs(touchDeltaY / 100.0f));
+       //         spawned = false;
+       //     }
+           
+       // }
+        
+        //spawned = true;
+
+    }
+
+    void SpawnSpaceship(float initVel)
+    {
+        spawnPosition = transform.position + (transform.forward * offset);
+        Omnipotentv3 = Instantiate(Omnipotentv3, spawnPosition, transform.rotation);
+
+        //Makes the new Planetoid a child of the ImageTarget object so it is rendered in the AR scene
+        //planetoid.transform.SetParent(ImageTarget);
+
+        //Access the Gravity script of the newly spawned spaceship and set the Attractor Rigidbody to the "Sun" GameObject
+        Omnipotentv3.GetComponent<Gravity>().rbAttractor = rbAttractor;
+
+        //Sets a random color for each Planetoid's tail
+        //Omnipotentv2.GetComponent<TrailRenderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+        //Set the intensity of the spaceship's initial velocity to the change in Y of the touch position
+        Omnipotentv3.GetComponent<InitalVelocity>().forceIntensity = initVel;
+
+    }
+
+}
---
+++ ARKit_gARvity-master/ARKit_gARvity-master/ARKit_gARvity Mk. I/Assets/ShipSteering.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSteering : MonoBehaviour {
+
+	// Use this for initialization
+
+        private Touch touch;
+        private static Vector2 initialTouchPosition;
+        private float touchDeltaY;
+        private float touchDeltaX;
+        //private bool touchLock = false;
+        private static bool mouseDown = false;
+        private Vector3 tooFastX = new Vector3(5000.0f, 0.0f, 0.0f);
+        private Vector3 tooFastY = new Vector3(0.0f, 5000.0f, 0.0f);
+        private Vector3 tooFastZ = new Vector3(0.0f, 0.0f, 5000.0f);
+
+    // MOUSE:
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody ship = GetComponent<Rigidbody>();
+        ship.useGravity = false;
+        if (Input.GetMouseButtonDown(0))
+        {
+            initialTouchPosition = touch.position;
+            mouseDown = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseDown = false;
+        }
+        if (mouseDown == true)
+        {
+            touchDeltaY = Input.mousePosition.y - initialTouchPosition.y;
+            touchDeltaX = initialTouchPosition.x - Input.mousePosition.x;
+            Debug.Log((touchDeltaY));
+            Debug.Log((touchDeltaX));
+            ship.AddForce(-touchDeltaX / 200.0f, touchDeltaY / 200.0f, 0, ForceMode.Acceleration);
+            ship.transform.Rotate(transform.right, touchDeltaY / 100.0f);
+            ship.transform.Rotate(transform.forward, touchDeltaX / 100.0f);
+        }
+        if(ship.GetPointVelocity(ship.transform.position).x > tooFastX.x)
+        {
+            ship.AddForce(-100.0f, 0.0f, 0.0f, ForceMode.Force);
+        }
+        if (ship.GetPointVelocity(ship.transform.position).y > tooFastY.y)
+        {
+            ship.AddForce(0.0f, -100.0f, 0.0f, ForceMode.Force);
+        }
+        if (ship.GetPointVelocity(ship.transform.position).z > tooFastZ.z)
+        {
+            ship.AddForce(0.0f, 0.0f, -100.0f, ForceMode.Force);
+        }
+    }
+    
+    // TOUCH:
+    // Update is called once per frame
+    //void Update () {
+    //    touch = Input.GetTouch(0);
+
+    //    switch (touch.phase)
+    //    {
+    //        case TouchPhase.Began:
+
+    //            if (!touchLock)
+    //            {
+
+    //                initialTouchPosition = touch.position;
+
+    //                touchLock = true;
+    //            }
+    //            break;
+
+    //        case TouchPhase.Moved:
+    //            touchDeltaY = initialTouchPosition.y - touch.position.y;
+    //            touchDeltaX = initialTouchPosition.x - touch.position.x;
+    //            Debug.Log((touchDeltaY));
+    //            Debug.Log((touchDeltaX));
+    //            ship.AddForce(touchDeltaX/100.0f, touchDeltaY/100.0f, 0);
+    //            ship.transform.Rotate(transform.right, touchDeltaY/100.0f);
+    //            ship.transform.Rotate(transform.forward, touchDeltaX/100.0f);
+    //            break;
+
+    //        case TouchPhase.Ended:
+
+    //            //SpawnSpaceship(Mathf.Abs(touchDeltaY / 100.0f));
+
+    //            touchLock = false;
+    //            break;
+    //    }
+    //}
+    }
+
